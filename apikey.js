de9af968f3487bdf259bb3201d6ae32c0fff665d
정유진 <jane320@naver.com>
2023-10-11 11:03:03 +0900

    api키 추가

---
+++ apikey.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyAFg0Dx_PN_87S2K5NgZ7NX9MkJYaJNFpE",
+    authDomain: "guestbook-71323.firebaseapp.com",
+    projectId: "guestbook-71323",
+    storageBucket: "guestbook-71323.appspot.com",
+    messagingSenderId: "1012499364192",
+    appId: "1:1012499364192:web:eba0e7846eba681b050605"
+}
